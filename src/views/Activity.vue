deeae1b835db901deea8e2c5097d82ad216f9072
Jeamar <jeamar1234@gmail.com>
2019-03-27 15:06:47 +0800

    done submit e-claim page

--- src/views/Activity.vue
+++ src/views/Activity.vue
@@ -1,6 +1,17 @@
 <template>
 	<div class="activity-container">
 		Activity Page
+		<vue-slider 
+		  class="range-slider"
+			v-model="range_values" 
+      :marks="range_marks"
+      :enableCross="false"
+      :min="1"
+      :max="12"
+      :tooltip="'none'"
+     >
+      
+    </vue-slider>
 	</div>
 </template>
 
