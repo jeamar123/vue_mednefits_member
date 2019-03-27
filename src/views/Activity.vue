90994a3fa2985581b2a7b0a9509aa8eedb8ad8a2
noelou jan <noelounagac@gmail.com>
2019-03-27 10:30:47 +0800

    add some text for activity page

--- src/views/Activity.vue
+++ src/views/Activity.vue
@@ -1,6 +1,11 @@
 <template>
 	<div class="activity-container">
-		Activity Page
+		<div class="activity-header-navigation">
+			<div>Select a timeframe</div>
+			<div class="year-selector">This year</div>
+			<div class="year-selector">Last year</div>
+			<div class="year-selector">Custom</div>
+		</div>
 	</div>
 </template>
 
